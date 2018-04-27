24a1ef8ed59c5006be2074bf7bb1fc55a2f3e9c3
Rafael Villar Burke <pachi@ietcc.csic.es>
2018-04-27 17:11:20 +0200

    types: comprobar que todos los componentes tienen el mismo número de valores al convertir

--- src/types.rs
+++ src/types.rs
@@ -509,6 +509,15 @@ impl str::FromStr for TComponents {
         let cdata = datalines
             .map(|e| e.parse())
             .collect::<Result<Vec<TComponent>, _>>()?;
+        {
+            let cdata_lens: Vec<_> = cdata.iter().map(|e| e.values.len()).collect();
+            if &cdata_lens.iter().max().unwrap() != &cdata_lens.iter().min().unwrap() {
+                return Err(format_err!(
+                    "Energy components have different number of values: {:?}",
+                    cdata_lens
+                ));
+            }
+        }
         Ok(TComponents { cmeta, cdata })
     }
 }
